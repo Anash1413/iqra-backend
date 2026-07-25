e6b12ab1a47c39512aa52cef64955bfb1b26bcfc
MAviz Ahmad <ak9399581@gmail.com>
2026-07-26 02:00:51 +0530

    3rd

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const app = express();
 
 // Standard middleware
 app.use(cors({
-  origin: ["http://localhost:5173","https://iqra-amdara.netlify.app", "http://localhost:5174"],
+  origin: '*',
+  // origin: ["http://localhost:5173","https://iqra-amdara.netlify.app", "http://localhost:5174"],
   credentials: true,
 }));
 app.use(express.json());
